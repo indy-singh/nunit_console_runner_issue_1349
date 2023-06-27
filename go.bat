70e88e51e46866c3cb36bc2dc4fbfd26636a74a2
Indy <1906778+indy-singh@users.noreply.github.com>
2023-06-27 19:47:56 +0100

    Update go.bat

--- go.bat
+++ go.bat
@@ -1,5 +1,5 @@
 @echo off
-choco install -y nunit-console-runner --version=3.16.3 -Y
+choco install -y nunit-console-runner --version=3.16.3
 choco install -y nunit-extension-vs-project-loader --version=3.9.0
 dotnet build
 nunit3-console.exe .\bin\Debug\net472\nunit_console_runner_issue_1349.dll
