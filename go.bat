1a5a35f420d731f6390c86de5536f8947d224c1e
indy-singh <1906778+indy-singh@users.noreply.github.com>
2023-06-27 19:45:03 +0100

    Make it easy to repo

---
+++ go.bat
@@ -0,0 +1,7 @@
+@echo off
+choco install -y nunit-console-runner --version=3.16.3 -Y
+choco install -y nunit-extension-vs-project-loader --version=3.9.0
+dotnet build
+nunit3-console.exe .\bin\Debug\net472\nunit_console_runner_issue_1349.dll
+nunit3-console.exe .\bin\Debug\net6.0\nunit_console_runner_issue_1349.dll
+pause
